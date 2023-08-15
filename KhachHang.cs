aa70008d026aa39132be3d54a2482dba4a14cb8d
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-08-15 15:46:19 +0700

    Add files via upload

---
+++ KhachHang.cs
@@ -0,0 +1,81 @@
+﻿using bt1.customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1.customer
+{
+    public class KhachHang
+    {
+        public string MaKhachHang { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgayRaHoaDon { get; set; }
+    }
+
+    public class KhachHangVietNam : KhachHang
+    {
+        public enum DoiTuongKhachHang
+        {
+            SinhHoat,
+            KinhDoanh,
+            SanXuat
+        }
+
+        public DoiTuongKhachHang DoiTuong { get; set; }
+        public double SoLuong { get; set; }
+        public double DonGia { get; set; }
+
+        public double TinhThanhTien()
+        {
+            double thanhTien = 0;
+            double donGiaMoi = 0;
+           
+
+            if (SoLuong <= 50)
+            {
+                
+                donGiaMoi = 1000;
+                thanhTien = SoLuong * donGiaMoi;
+            }
+            else if (SoLuong <= 100)
+            {
+                
+                donGiaMoi = 1200;
+                thanhTien =50000 + (SoLuong - 50) * donGiaMoi;
+            }
+            else if (SoLuong <= 200)
+            {
+                
+                donGiaMoi = 1500;
+                thanhTien = 110000 + (SoLuong - 100) * donGiaMoi;
+            }
+            else
+            {
+               
+                donGiaMoi = 2000;
+                thanhTien = 260000 + (SoLuong - 200) * donGiaMoi;
+            }
+
+
+            return thanhTien;
+        }
+    }
+
+    public class KhachHangNuocNgoai : KhachHang
+    {
+        public string QuocTich { get; set; }
+        public double SoLuong { get; set; }
+        public double DonGia { get; set; }
+
+        public double TinhThanhTien()
+        {
+            return SoLuong * DonGia;
+        }
+    }
+}
+
+ 
+
+
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using bt1.customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bt1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<KhachHang> danhSachHoaDon = new List<KhachHang>();
+
+            // Thêm khách hàng Việt Nam vào danh sách hoá đơn
+            KhachHangVietNam khachVietNam = new KhachHangVietNam
+            {
+                MaKhachHang = "VN001",
+                HoTen = "Nguyen Van A",
+                NgayRaHoaDon = new DateTime(2023, 8, 8),
+                DoiTuong = KhachHangVietNam.DoiTuongKhachHang.SinhHoat,
+                SoLuong = 90,
+                DonGia = 1000                           
+            };
+            danhSachHoaDon.Add(khachVietNam);
+
+            // Thêm khách hàng nước ngoài vào danh sách hoá đơn
+            KhachHangNuocNgoai khachNuocNgoai = new KhachHangNuocNgoai
+            {
+                MaKhachHang = "NN001",
+                HoTen = "John Smith",
+                NgayRaHoaDon = new DateTime(2023, 8, 8),
+                QuocTich = "USA",
+                SoLuong = 80,
+                DonGia = 2000
+            };
+            danhSachHoaDon.Add(khachNuocNgoai);
+
+            // In thông tin và thành tiền của các khách hàng
+            foreach (var khachHang in danhSachHoaDon)
+            {
+                Console.WriteLine($"Mã KH: {khachHang.MaKhachHang}");
+                Console.WriteLine($"Họ tên: {khachHang.HoTen}");
+                Console.WriteLine($"Ngày ra hoá đơn: {khachHang.NgayRaHoaDon.ToString("dd/MM/yyyy")}");
+
+                if (khachHang is KhachHangVietNam)
+                {
+                    var khachHangVietNam = khachHang as KhachHangVietNam;
+                    Console.WriteLine($"Đối tượng: {khachVietNam.DoiTuong}");
+                    Console.WriteLine($"Số lượng: {khachVietNam.SoLuong}");
+                    Console.WriteLine($"Đơn giá: {khachVietNam.DonGia}");
+                    Console.WriteLine($"Thành tiền: {khachVietNam.TinhThanhTien()}");
+                }
+                else if (khachHang is KhachHangNuocNgoai)
+                {
+                    var khachHanNuocNgoai = khachHang as KhachHangNuocNgoai;
+                    Console.WriteLine($"Quốc tịch: {khachNuocNgoai.QuocTich}");
+                    Console.WriteLine($"Số lượng: {khachNuocNgoai.SoLuong}");
+                    Console.WriteLine($"Đơn giá: {khachNuocNgoai.DonGia}");
+                    Console.WriteLine($"Thành tiền: {khachNuocNgoai.TinhThanhTien()}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
